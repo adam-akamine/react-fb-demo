bbd698bc1dcfb6f448ac8e3f979670a2e603d6e6
kekuso <kekuso808@gmail.com>
2016-08-13 14:37:22 -1000

    Setting up form and submission

--- public/app.js
+++ public/app.js
@@ -1,16 +1,48 @@
 const CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+    let author = this.state.author.trim();
+    let text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.setState({author: '', text: ''});
+  },
   render: function() {
     return (
-      <div className = "commentForm">
-        Hello, world! CommentForm!
-      </div>
+      <form className = "commentForm" onSubmit={this.handleSubmit}>
+         <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+        <h1>{this.state.author}</h1>
+        <h1>{this.state.text}</h1>
+      </form>
     );
   }
 });
 
 const Comment = React.createClass({
   rawMarkup: function () {
-    var md = new Remarkable();
+    let md = new Remarkable();
     const rawMarkup = md.render(this.props.children.toString());
     return { __html: rawMarkup };
   },
@@ -82,10 +114,10 @@ const CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentForm />
         <CommentList
           data = { this.state.data }
         />
+        <CommentForm />
       </div>
     );
   }
